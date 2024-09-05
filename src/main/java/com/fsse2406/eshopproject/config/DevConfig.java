8347113055752caab0176fcf155d6e1964db33f5
vldicky <vlchandicky@gmail.com>
2024-09-05 23:16:44 +0800

    Updated CartItemResponseDto 25Aug & Updated CORS DevConfig(Url 5173) & SecurityConfig on 02Sep, Final on 04Sep

--- src/main/java/com/fsse2406/eshopproject/config/DevConfig.java
+++ src/main/java/com/fsse2406/eshopproject/config/DevConfig.java
@@ -1,6 +1,6 @@
 package com.fsse2406.eshopproject.config;
 
 public class DevConfig {
-    public static final String DEV_BSE_URL="http://localhost:8080";
+    public static final String DEV_BSE_URL="http://localhost:5173";
     public static final String PROD_BSE_URL="http://fsse2406-project-vldicky.s3-website-ap-southeast-1.amazonaws.com";
 }
